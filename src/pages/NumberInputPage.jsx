510189296fbf3841ebb5e6e019c6bcd3c01acf11
Kseniia <ksenia.guseva789@gmail.com>
2023-10-24 13:58:27 +0300

    keypad navig enabled

--- src/pages/NumberInputPage.jsx
+++ src/pages/NumberInputPage.jsx
@@ -1,41 +1,231 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useEffect, useRef, createRef } from "react";
+
 import { useNavigate } from "react-router-dom";
 import { InputMask } from "@react-input/mask";
+import cn from 'classnames';
+import CloseButton from "../components/CloseButton";
 
 const NumberInputPage = () => {
   const [inputValue, setValue] = useState("+7(___)___-__-__");
-  const onChange = (e) => setValue(e.target.value);
+  const [checkBox, setCheckBox] = useState(false);
+  const onChange = (e) => {
+    setValue(e.target.value);
+  };
+  const handleCheckBox = () => {
+    const newCheckBox = !checkBox;
+    setCheckBox(newCheckBox);
+  };
   const navigate = useNavigate();
 
   const phoneInputRef = useRef(null);
+  const submitBtnRef = useRef(null);
+  const btn1Ref = useRef(null);
+  const btn2Ref = useRef(null);
+  const btn3Ref = useRef(null);
+  const btn4Ref = useRef(null);
+  const btn5Ref = useRef(null);
+  const btn6Ref = useRef(null);
+  const btn7Ref = useRef(null);
+  const btn8Ref = useRef(null);
+  const btn9Ref = useRef(null);
+  const btn0Ref = useRef(null);
+  const deleteRef = useRef(null);
+  const closeBtnRef = createRef(null);
+  const checkmarkRef = useRef(null);
 
-  useEffect(() => {
-    if (phoneInputRef.current && phoneInputRef.current.element) {
-      phoneInputRef.current.element.setSelectionRange(2, 3);
-      phoneInputRef.current.element.focus();
+  // useEffect(() => {
+  //   if (phoneInputRef.current && phoneInputRef.current.element) {
+  //     phoneInputRef.current.element.setSelectionRange(2, 3);
+  //     // phoneInputRef.current.element.focus();
+  //   }
+  // }, []);
+
+  const valuesAndRefs = {
+    1: btn1Ref,
+    2: btn2Ref,
+    3: btn3Ref,
+    4: btn4Ref,
+    5: btn5Ref,
+    6: btn6Ref,
+    7: btn7Ref,
+    8: btn8Ref,
+    9: btn9Ref,
+    0: btn0Ref,
+    checkmarkkBox: checkmarkRef,
+    submitBtn: submitBtnRef,
+    closeButton: closeBtnRef,
+  }
+
+  const handleKeyDown = (e) => {
+    console.log(e.target);
+    const value = e.target.getAttribute("data-value");
+    console.log(value);
+    const action = e.code;
+    if (action === 'Enter') {
+      e.preventDefault();
     }
-  }, []);
+    console.log(action);
+    const add1 = (value) => {
+      const newNum = Number(value) + 1;
+      valuesAndRefs[newNum].current.focus();
+    }
+    const add3 = (value) => {
+      const newNum = Number(value) + 3;
+      valuesAndRefs[newNum].current.focus();
+    };
+    const deduct1 = (value) => {
+      const newNum = Number(value) - 1;
+      valuesAndRefs[newNum].current.focus();
+    };
+    const deduct3 = (value) => {
+      const newNum = Number(value) - 3;
+      valuesAndRefs[newNum].current.focus();
+    };
 
-  const submitNumber = (e) => {
-    e.preventDefault();
-    // логика для отправки данных на сервер
-    navigate("/submitted");
+    const chooseNum = (value) => {
+      e.preventDefault();
+      if (value === "delete") {
+        if (inputValue === "+7(___)___-__-__") {
+          return null;
+        }
+        const newValueAfterDelete = inputValue.replace(/\d(?=[^\d]*$)/, "_");
+        setValue(newValueAfterDelete);
+        return null;
+      }
+      const newValue = inputValue.replace("_", value);
+      setValue(newValue);
+      valuesAndRefs[value].current.focus();
+    };
+
+    const constuctorFunc = {
+      phoneInput: {
+        ArrowDown: () => btn1Ref.current.focus(),
+        ArrowUp: () => closeBtnRef.current.focus(),
+      },
+      1: {
+        ArrowDown: add3,
+        ArrowUp: () => phoneInputRef.current.focus(),
+        ArrowLeft: () => phoneInputRef.current.focus(),
+        ArrowRight: add1,
+        Enter: chooseNum,
+      },
+      2: {
+        ArrowDown: add3,
+        ArrowUp: () => phoneInputRef.current.focus(),
+        ArrowLeft: deduct1,
+        ArrowRight: add1,
+        Enter: chooseNum,
+      },
+      3: {
+        ArrowDown: add3,
+        ArrowUp: () => phoneInputRef.current.focus(),
+        ArrowLeft: deduct1,
+        ArrowRight: add1,
+        Enter: chooseNum,
+      },
+      4: {
+        ArrowDown: add3,
+        ArrowUp: deduct3,
+        ArrowLeft: deduct1,
+        ArrowRight: add1,
+        Enter: chooseNum,
+      },
+      5: {
+        ArrowDown: add3,
+        ArrowUp: deduct3,
+        ArrowLeft: deduct1,
+        ArrowRight: add1,
+        Enter: chooseNum,
+      },
+      6: {
+        ArrowDown: add3,
+        ArrowUp: deduct3,
+        ArrowLeft: deduct1,
+        ArrowRight: add1,
+        Enter: chooseNum,
+      },
+      7: {
+        ArrowDown: () => deleteRef.current.focus(),
+        ArrowUp: deduct3,
+        ArrowLeft: deduct1,
+        ArrowRight: add1,
+        Enter: chooseNum,
+      },
+      8: {
+        ArrowDown: () => deleteRef.current.focus(),
+        ArrowUp: deduct3,
+        ArrowLeft: deduct1,
+        ArrowRight: add1,
+        Enter: chooseNum,
+      },
+      9: {
+        ArrowDown: () => btn0Ref.current.focus(),
+        ArrowUp: deduct3,
+        ArrowLeft: deduct1,
+        ArrowRight: () => deleteRef.current.focus(),
+        Enter: chooseNum,
+      },
+      0: {
+        ArrowDown: () => checkmarkRef.current.focus(),
+        ArrowUp: () => btn9Ref.current.focus(),
+        ArrowLeft: () => deleteRef.current.focus(),
+        ArrowRight: () => checkmarkRef.current.focus(),
+        Enter: chooseNum,
+      },
+      delete: {
+        ArrowDown: () => checkmarkRef.current.focus(),
+        ArrowUp: () => btn7Ref.current.focus(),
+        ArrowLeft: () => btn9Ref.current.focus(),
+        ArrowRight: () => btn0Ref.current.focus(),
+        Enter: chooseNum,
+      },
+      checkmark: {
+        ArrowDown: () => submitBtnRef.current.focus(),
+        ArrowUp: () => deleteRef.current.focus(),
+        ArrowLeft: () => btn0Ref.current.focus(),
+        ArrowRight: () => submitBtnRef.current.focus(),
+      },
+      submitBtn: {
+        ArrowDown: () => phoneInputRef.current.focus(),
+        ArrowUp: () => checkmarkRef.current.focus(),
+        ArrowLeft: () => checkmarkRef.current.focus(),
+        ArrowRight: () => closeBtnRef.current.focus(),
+      },
+      closeButton: {
+        ArrowLeft: () => phoneInputRef.current.focus(),
+      }
+
+    };
+    
+    if (Object.hasOwn(constuctorFunc[value], action)) {
+        constuctorFunc[value][action](value);
+      }
   };
+  
 
   const numPadClick = (e) => {
     const value = e.target.getAttribute("data-value");
-    if (value === "Стереть") {
+    if (value === "delete") {
       if (inputValue === "+7(___)___-__-__") {
         return null;
       }
       const newValueAfterDelete = inputValue.replace(/\d(?=[^\d]*$)/, "_");
-      console.log(newValueAfterDelete);
       setValue(newValueAfterDelete);
       return null;
     }
     const newValue = inputValue.replace("_", value);
     setValue(newValue);
   };
+
+    const submitNumber = (e) => {
+      e.preventDefault();
+      if (checkBox && !inputValue.includes('_')) {
+        // логика для отправки данных на сервер
+      navigate("/submitted");
+      return;
+      }
+      return null;
+    };
 
   return (
     <>
@@ -57,37 +247,52 @@ const NumberInputPage = () => {
               autoFocus
               onFocus={(e) => e.target.setSelectionRange(2, 3)}
               ref={phoneInputRef}
+              onKeyDown={handleKeyDown}
+              data-value="phoneInput"
             />
             <p className="form__text">
               и с Вами свяжется наш менеждер для <br></br> дальнейшей
               консультации
             </p>
 
-            <label htmlFor="personalData" className="checkboxContainer">
+            <label htmlFor="personalData" className="checkboxContainer" >
               Согласие на обработку<br></br>персональных данных
               <input
                 type="checkbox"
                 name="personalData"
                 id="personalData"
                 required
+                onClick={handleCheckBox}
+                ref = {checkmarkRef}
+              onKeyDown={handleKeyDown}
+              data-value='checkmark'
               />
-              <span className="checkmark"></span>
+              <div className="checkmark" 
+              
+                ></div>
             </label>
             <button
-              className="btn"
+              className={cn("btn", {
+                'btn-enabled': checkBox && !inputValue.includes('_'),
+                'btn-disabled': !checkBox || inputValue.includes('_'),
+              })}
               type="submit"
               id="button-addon2"
-              // ref={submitEl}
+              ref={submitBtnRef}
+              onKeyDown={handleKeyDown}
+              data-value="submitBtn"
             >
               ПОДТВЕРДИТЬ НОМЕР
             </button>
           </form>
-          <div className="numpad">
+          <div className="numpad" tabIndex="0">
+            
             <button
-              type="button"
               className="numpad__btn"
               onClick={numPadClick}
               data-value="1"
+              ref = {btn1Ref}
+              onKeyDown={handleKeyDown}
             >
               1
             </button>
@@ -95,6 +300,8 @@ const NumberInputPage = () => {
               className="numpad__btn"
               onClick={numPadClick}
               data-value="2"
+              ref = {btn2Ref}
+              onKeyDown={handleKeyDown}
             >
               2
             </button>
@@ -102,6 +309,8 @@ const NumberInputPage = () => {
               className="numpad__btn"
               onClick={numPadClick}
               data-value="3"
+              ref = {btn3Ref}
+              onKeyDown={handleKeyDown}
             >
               3
             </button>
@@ -109,6 +318,8 @@ const NumberInputPage = () => {
               className="numpad__btn"
               onClick={numPadClick}
               data-value="4"
+              ref= {btn4Ref}
+              onKeyDown={handleKeyDown}
             >
               4
             </button>
@@ -116,6 +327,8 @@ const NumberInputPage = () => {
               className="numpad__btn"
               onClick={numPadClick}
               data-value="5"
+              ref = {btn5Ref}
+              onKeyDown={handleKeyDown}
             >
               5
             </button>
@@ -123,6 +336,8 @@ const NumberInputPage = () => {
               className="numpad__btn"
               onClick={numPadClick}
               data-value="6"
+              ref = {btn6Ref}
+              onKeyDown={handleKeyDown}
             >
               6
             </button>
@@ -130,6 +345,8 @@ const NumberInputPage = () => {
               className="numpad__btn"
               onClick={numPadClick}
               data-value="7"
+              ref = {btn7Ref}
+              onKeyDown={handleKeyDown}
             >
               7
             </button>
@@ -137,6 +354,8 @@ const NumberInputPage = () => {
               className="numpad__btn"
               onClick={numPadClick}
               data-value="8"
+              ref = {btn8Ref}
+              onKeyDown={handleKeyDown}
             >
               8
             </button>
@@ -144,20 +363,26 @@ const NumberInputPage = () => {
               className="numpad__btn"
               onClick={numPadClick}
               data-value="9"
+              ref = {btn9Ref}
+              onKeyDown={handleKeyDown}
             >
               9
             </button>
             <button
               className="numpad__btn"
               onClick={numPadClick}
-              data-value="Стереть"
+              data-value="delete"
+              ref = {deleteRef}
+              onKeyDown={handleKeyDown}
             >
               СТЕРЕТЬ
-            </button>
+            </button>     
             <button
               className="numpad__btn"
               onClick={numPadClick}
               data-value="0"
+              ref = {btn0Ref}
+              onKeyDown={handleKeyDown}
             >
               0
             </button>
@@ -165,23 +390,10 @@ const NumberInputPage = () => {
         </div>
         <p className="qr__text">СКАНИРУЙТЕ QR-КОД ДЛЯ ПОЛУЧЕНИЯ ДОПОЛНИТЕЛЬНОЙ ИНФОРМАЦИИ</p>
         <img
-          className="card__qr"
+          className="card__qr numberInputPage_card__qr"
           alt="qr code"
-          src="../../public/videoBG.png"
         />
-        <button className="btn closeBtn"><svg xmlns="http://www.w3.org/2000/svg" width="88" height="52" viewBox="0 0 88 52" fill="none">
-  <g clipPath="url(#clip0_190_36741)">
-    <rect width="88" height="52" transform="matrix(1 0 0 -1 0 52)" fill="white"/>
-    <line x1="34.3448" y1="14.9407" x2="54.6264" y2="35.2223" stroke="black" strokeWidth="3"/>
-    <line x1="33.6576" y1="35.2223" x2="53.9392" y2="14.9407" stroke="black" strokeWidth="3"/>
-  </g>
-  <rect x="1" y="1" width="86" height="50" stroke="black" strokeWidth="2"/>
-  <defs>
-    <clipPath id="clip0_190_36741">
-      <rect width="88" height="52" fill="white"/>
-    </clipPath>
-  </defs>
-</svg></button>
+        <CloseButton ref={closeBtnRef} onKeyDown={handleKeyDown} data-value="closeButton"/>
       </div>
     </>
   );
--- src/pages/VideoBannerPage.jsx
+++ src/pages/VideoBannerPage.jsx
@@ -11,11 +11,11 @@ const VideoBannerPage = () => {
         <div className="backgroundFirst">
             <div className="card banner__card">
                 <div className="bannerCard__header">
-                <p>ИСПОЛНИТЕ МЕЧТУ ВАШЕГО МАЛЫША! <br></br> ПОДАРИТЕ ЕМУ СОБАКУ!</p>
+                <p className="bannerCard__text">ИСПОЛНИТЕ МЕЧТУ ВАШЕГО МАЛЫША! <br></br> ПОДАРИТЕ ЕМУ СОБАКУ!</p>
                 </div>
-                <img className="card__qr" alt="qr code" src="../../public/videoBG.png"/>
+                <img className="card__qr" alt="qr code" src="../../public/qr-code.png"/>
                 <p className="card__text bannerCard__text">Сканируйте QR-код <br></br> или нажмите ОК</p>
-                <button className='btn card__button active' onClick={goNext}>ОК</button>
+                <button className='btn card__button active' onClick={goNext} autoFocus>ОК</button>
             </div>
         </div>
         </>
